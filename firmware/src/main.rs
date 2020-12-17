ee513f5243dbc5c6d410e94d155af0a2449f1750
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-12-17 08:47:22 +0100

    RTIC version using reed switch

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -5,15 +5,10 @@ use nrf51_hal as hal;
 use panic_halt as _;
 
 extern crate cortex_m;
-use cortex_m_rt::entry;
 
-use embedded_hal::adc::OneShot;
-use embedded_hal::digital::v2::OutputPin;
-
-use hal::adc::{Adc, AdcConfig};
-use hal::clocks::Clocks;
-use hal::gpio::Level;
-use hal::rtc::{Rtc, RtcInterrupt};
+use hal::gpio::{Input, Pin, PullUp};
+use hal::gpiote::*;
+use rtic::app;
 
 use log::LevelFilter;
 use rtt_logger::RTTLogger;
@@ -21,70 +16,52 @@ use rtt_target::rtt_init_print;
 
 static LOGGER: RTTLogger = RTTLogger::new(LevelFilter::Info);
 
-#[entry]
-fn main() -> ! {
-    rtt_init_print!();
-
-    unsafe {
-        log::set_logger_racy(&LOGGER).unwrap();
+#[app(device = crate::hal::pac, peripherals = true)]
+const APP: () = {
+    struct Resources {
+        gpiote: Gpiote,
+        input: Pin<Input<PullUp>>,
     }
-    log::set_max_level(log::LevelFilter::Info);
 
-    let p = hal::pac::Peripherals::take().unwrap();
-    let gpio = hal::gpio::p0::Parts::new(p.GPIO);
+    #[init]
+    fn init(mut ctx: init::Context) -> init::LateResources {
+        rtt_init_print!();
 
-    let mut input = gpio.p0_01.into_floating_input();
-    let mut output = gpio.p0_07.into_push_pull_output(Level::Low);
-
-    // Enable lazer
-    let _ = output.set_high().unwrap();
-
-    let config = AdcConfig {
-        input_selection: hal::pac::adc::config::INPSEL_A::ANALOGINPUTNOPRESCALING,
-        resolution: hal::pac::adc::config::RES_A::_10BIT,
-        reference: hal::pac::adc::config::REFSEL_A::VBG,
-    };
-    let mut adc = Adc::new(p.ADC, config);
-
-    let clocks = Clocks::new(p.CLOCK).enable_ext_hfosc();
-    let _clocks = clocks.start_lfclk();
-
-    let mut rtc = Rtc::new(p.RTC0, 4095).unwrap();
-    rtc.enable_event(RtcInterrupt::Tick);
-    rtc.enable_counter();
-
-    log::info!("Started application");
-
-    let mut count = 0;
-    let mut goal = false;
-    loop {
-        while !rtc.is_event_triggered(RtcInterrupt::Tick) {}
-        rtc.reset_event(RtcInterrupt::Tick);
-        rtc.clear_counter();
-        count += 1;
-
-        let value = adc.read(&mut input).unwrap();
-        if value > 900 {
-            if !goal {
-                log::info!("GOAL!!!!");
-                goal = true;
-            }
-        } else {
-            goal = false;
+        unsafe {
+            log::set_logger_racy(&LOGGER).unwrap();
         }
+        log::set_max_level(log::LevelFilter::Info);
 
-        /*
-        if count % 2 < 25 {
-            if !on {
-                on = true;
-                log::info!("Laser on!");
-            }
-        } else {
-            if on {
-                on = false;
-                log::info!("Laser off!");
-            }
-            let _ = output.set_low().unwrap();
-        }*/
+        let gpio = hal::gpio::p0::Parts::new(ctx.device.GPIO);
+
+        let mut input = gpio.p0_01.into_pullup_input().degrade();
+
+        //    let _clocks = Clocks::new(p.CLOCK).enable_ext_hfosc();
+
+        let mut gpiote = Gpiote::new(ctx.device.GPIOTE);
+        gpiote
+            .channel0()
+            .input_pin(&input)
+            .hi_to_lo()
+            .enable_interrupt();
+
+        log::info!("Initialized application");
+        init::LateResources { gpiote, input }
     }
-}
+
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        log::info!("Started application");
+        loop {
+            cortex_m::asm::wfi();
+        }
+    }
+
+    #[task(binds = GPIOTE, resources = [gpiote])]
+    fn on_detected(ctx: on_detected::Context) {
+        if ctx.resources.gpiote.channel0().is_event_triggered() {
+            log::info!("GOAL!!");
+        }
+        ctx.resources.gpiote.reset_events();
+    }
+};
