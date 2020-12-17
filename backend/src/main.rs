dd9702ad14e4a9c7752516bb14efa8a3043126b7
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-12-17 08:46:32 +0100

    Backend snapshot

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,9 +4,8 @@ extern crate log;
 use actix_web::dev::HttpResponseBuilder;
 use actix_web::http::StatusCode;
 use actix_web::{post, web, App, HttpRequest, HttpResponse, HttpServer};
-use cloudevents::{EventBuilder, EventBuilderV10};
-use cloudevents_sdk_actix_web::{HttpRequestExt, HttpResponseBuilderExt};
-use cloudevents_sdk_reqwest::RequestBuilderExt;
+use cloudevents_sdk_actix_web::HttpRequestExt;
+use std::collections::HashMap;
 
 #[post("/")]
 async fn reply_event(
@@ -14,60 +13,97 @@ async fn reply_event(
     payload: web::Payload,
 ) -> Result<HttpResponse, actix_web::Error> {
     let request_event = req.to_event(payload).await?;
-    info!("{}", request_event);
+    info!("{:?}", request_event);
 
-    // Build response event cloning the original event and setting the new type and source
-    let response_event = EventBuilderV10::from(request_event)
-        .source("https://github.com/knative/docs/docs/serving/samples/cloudevents/cloudevents-rust")
-        .ty("dev.knative.docs.sample")
-        .build()
-        // If i can't build the event, fail with internal server error
-        .map_err(actix_web::error::ErrorInternalServerError)?;
-
-    HttpResponseBuilder::new(StatusCode::OK)
-        .event(response_event)
-        .await
+    HttpResponseBuilder::new(StatusCode::OK).await
 }
 
-#[post("/")]
-async fn forward_event(
-    req: HttpRequest,
-    payload: web::Payload,
-    sink_url: web::Data<String>,
-) -> Result<HttpResponse, actix_web::Error> {
-    let sink_url: &str = &sink_url;
+type Score = u32;
+type Rating = u32;
+type GameId = u32;
 
-    let request_event = req.to_event(payload).await?;
-    info!("{}", request_event);
+struct Player {
+    rating: Rating,
+}
 
-    // Build response event cloning the original event and setting the new type and source
-    let forward_event = EventBuilderV10::from(request_event)
-        .source("https://github.com/knative/docs/docs/serving/samples/cloudevents/cloudevents-rust")
-        .ty("dev.knative.docs.sample")
-        .build()
-        // If i can't build the event, fail with internal server error
-        .map_err(actix_web::error::ErrorInternalServerError)?;
+struct GameState {
+    status: Status,
+    left: Score,
+    right: Score,
+}
 
-    let client = reqwest::Client::new();
-    let response = client.post(sink_url)
-        .event(forward_event)
-        .map_err(actix_web::error::ErrorInternalServerError)?
-        .send()
-        .await
-        // If something went wrong when sending the event, fail with internal server error
-        .map_err(actix_web::error::ErrorInternalServerError)?;
+/// Notify API that new game should be created and assigned an id.
+/// POST /games
+///   RESPONSE: {"id": 12}
 
-    if response.status().is_success() {
-        Ok(HttpResponseBuilder::new(StatusCode::ACCEPTED).finish())
-    } else {
-        Ok(
-            HttpResponseBuilder::new(StatusCode::INTERNAL_SERVER_ERROR).body(format!(
-                "Received from {} status code {}",
-                sink_url,
-                response.status().as_str()
-            )),
-        )
+/// Update game state during game
+/// PUT /games/12
+///   BODY: {"id": 12, "state": {"left": 0, "right": 4}}
+///   RESPONSE: {"status": 201}
+
+/// Update game state marking as finished
+/// PUT /games/12
+///   BODY: {"id": 12, "state": {"left": 0, "right": 5, "status": "finished"}}
+///   RESPONSE: {"status": 201}
+
+/// Register a new player
+/// POST /players
+///   BODY: {"name": "Hockeyhurricane"}
+///   RESPONSE: {"status": 201, player: {"id": 332, "name": "Hockeyhurricane"}}
+
+/// Read specific player
+/// GET /players/332
+///   BODY: {"id": "332"}
+///   RESPONSE: {"id": 332, "name": "Hockeyhurricane", "rating": 1333}
+
+/// Read all players
+/// GET /players
+///   RESPONSE: [{"id": 332, "name": "Hockeyhurricane", "rating": 1333}]
+
+/// Register a completed game, binding the result to the player stats.
+/// PUT /games/12/register
+///   BODY: {"id": 12, left: 332, right: 333}
+
+/// GET /games
+///   RESPONSE: [{"12", "left": 332, "right": 333, state: {"left": 0, "right": 5, "status": "finished"}}]
+///
+///
+///   
+///   "
+
+pub enum Status {
+    Started,
+    Finished,
+}
+
+struct Game {
+    id: GameId,
+    state: GameState,
+    left: Option<Player>,
+    right: Option<Player>,
+}
+
+struct GameController {
+    database: HashMap<GameId, Game>,
+}
+
+impl GameController {
+    fn new() -> GameController {
+        GameController {
+            database: HashMap::new(),
+        }
     }
+
+    fn new_game(&mut self) -> &mut Game {
+    }
+
+    fn get_game(&mut self, id: GameId) -> Option<&mut Game> {
+        self.database.get_mut(&id)
+    }
+
+    fn update(id: GameId, state: GameState) {
+    }
+
 }
 
 #[actix_rt::main]
@@ -86,16 +122,7 @@ async fn main() -> std::io::Result<()> {
     // Create the HTTP server
     HttpServer::new(|| {
         let app_base = App::new().wrap(actix_web::middleware::Logger::default());
-
-        // Get k_sink env
-        let k_sink = std::env::var("K_SINK").ok();
-
-        // If k_sink is configured, then we start the app in forward mode
-        // Otherwise, we set it in reply mode
-        match k_sink {
-            Some(sink) => app_base.data(sink).service(forward_event),
-            None => app_base.service(reply_event),
-        }
+        app_base.service(reply_event)
     })
     .bind(("127.0.0.1", port))?
     .workers(1)
