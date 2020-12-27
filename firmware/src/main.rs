bcd9a956312aed1a210f18bc82171db595317136
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-12-27 07:13:29 +0100

    Switch to use RTC for timing

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -9,18 +9,20 @@ extern crate cortex_m;
 use embedded_hal::digital::v2::InputPin;
 use embedded_hal::digital::v2::OutputPin;
 
-use core::mem;
 use hal::gpio::{Input, Level, Output, Pin, PullUp, PushPull};
 use hal::gpiote::*;
-use hal::pac::TIMER0;
+use hal::rtc::{Rtc, RtcCompareReg, RtcInterrupt};
 use rtic::app;
 
 use log::LevelFilter;
 use rtt_logger::RTTLogger;
 use rtt_target::rtt_init_print;
 
+mod game;
+use crate::game::*;
+
 static LOGGER: RTTLogger = RTTLogger::new(LevelFilter::Trace);
-const COOLDOWN: u32 = 10_000_000u32;
+const COOLDOWN: u32 = 8;
 
 pub struct Goal {
     input: Pin<Input<PullUp>>,
@@ -35,7 +37,7 @@ impl Goal {
     }
 
     fn is_active(&mut self) -> bool {
-        self.input.is_high().unwrap()
+        self.input.is_low().unwrap()
     }
 }
 
@@ -45,7 +47,8 @@ const APP: () = {
         gpiote: Gpiote,
         left: Goal,
         right: Goal,
-        timer: TIMER0,
+        game: Game,
+        rtc: Rtc<hal::pac::RTC0>,
         #[init(0)]
         now: u32,
         #[init(false)]
@@ -85,6 +88,11 @@ const APP: () = {
             .input_pin(&start_btn)
             .hi_to_lo()
             .enable_interrupt();
+        gpiote
+            .channel3()
+            .input_pin(&undo_btn)
+            .hi_to_lo()
+            .enable_interrupt();
 
         let left = Goal {
             input: input_left,
@@ -98,16 +106,19 @@ const APP: () = {
             active: 0,
         };
 
-        let mut timer = ctx.device.TIMER0;
-        timer.init();
-        timer.set_interrupt(1000000);
+        let mut rtc = Rtc::new(ctx.device.RTC0, 4095).unwrap();
+        rtc.enable_event(RtcInterrupt::Compare0);
+        let _ = rtc.set_compare(RtcCompareReg::Compare0, 10);
+
+        let game = Game::new();
 
         log::info!("Initialized application");
         init::LateResources {
             gpiote,
             left,
             right,
-            timer,
+            rtc,
+            game,
         }
     }
 
@@ -119,21 +130,20 @@ const APP: () = {
         }
     }
 
-    #[task(binds = TIMER0, resources = [timer, left, right, now, started])]
-    fn on_timer(ctx: on_timer::Context) {
-        let on_timer::Resources {
-            timer,
+    #[task(binds = RTC0, resources = [rtc, left, right, now, started])]
+    fn on_rtc(ctx: on_rtc::Context) {
+        let on_rtc::Resources {
+            rtc,
             left,
             right,
             now,
             started,
         } = ctx.resources;
 
-        *now = timer.now();
-
+        *now += 1;
         let nownow = *now;
 
-        if timer.is_pending() && *started {
+        if *started {
             if left.active > 0 && nownow - left.active > COOLDOWN && !left.is_active() {
                 log::trace!("RESETTING LEFT COOLDOWN");
                 //left.led.set_high().unwrap();
@@ -145,105 +155,66 @@ const APP: () = {
                 right.active = 0;
             }
         }
-        timer.clear_interrupt();
-        timer.set_interrupt(nownow + 1000000);
+        rtc.reset_event(RtcInterrupt::Compare0);
+        rtc.clear_counter();
     }
 
-    #[task(binds = GPIOTE, resources = [gpiote, left, right, timer, started])]
+    #[task(binds = GPIOTE, resources = [gpiote, left, right, rtc, started, game, now])]
     fn on_detected(ctx: on_detected::Context) {
         let on_detected::Resources {
             gpiote,
-            timer,
             left,
             right,
             started,
+            game,
+            now,
+            rtc,
         } = ctx.resources;
 
         if gpiote.channel0().is_event_triggered() {
             log::trace!("INTERRUPT LEFT: {}", left.is_active());
-            if *started && left.active == 0 {
+            if *started && left.active == 0 && left.is_active() {
                 log::info!("GOAL PLAYER LEFT!!");
                 //  left.led.set_low().unwrap();
-                left.active = timer.now();
+                game.goal(Side::Left);
+                game.print();
+                left.active = *now;
             }
         } else if gpiote.channel1().is_event_triggered() {
             log::trace!("INTERRUPT RIGHT: {}", right.is_active());
-            if *started && right.active == 0 {
+            if *started && right.active == 0 && right.is_active() {
                 log::info!("GOAL PLAYER RIGHT!!");
                 //right.led.set_low().unwrap();
-                right.active = timer.now();
+                game.goal(Side::Right);
+                game.print();
+                right.active = *now;
             }
         } else if gpiote.channel2().is_event_triggered() {
             if !*started {
                 log::info!("Starting game!");
                 // TODO: Call game API
                 *started = true;
+                rtc.enable_interrupt(RtcInterrupt::Compare0, None);
+                rtc.reset_event(RtcInterrupt::Compare0);
+                rtc.clear_counter();
+                rtc.enable_counter();
             } else {
                 log::info!("Stopping game!");
                 // TODO: Call game API
                 *started = false;
+                game.reset();
                 right.reset();
                 left.reset();
+                rtc.disable_counter();
+                rtc.disable_interrupt(RtcInterrupt::Compare0, None);
+            }
+        } else if gpiote.channel3().is_event_triggered() {
+            if *started {
+                log::info!("Calling undo!");
+                game.undo();
+                game.print();
             }
         }
         gpiote.reset_events();
     }
 };
-
-pub trait Timer {
-    unsafe fn duplicate(&self) -> Self;
-
-    /// Initialize the timer so that it counts at a rate of 1 MHz.
-    fn init(&mut self);
-
-    /// Configures the timer's interrupt to fire at the given `Instant`.
-    fn set_interrupt(&mut self, at: u32);
-
-    /// Disables or acknowledges this timer's interrupt.
-    fn clear_interrupt(&mut self);
-
-    /// Returns whether a timer interrupt is currently pending.
-    ///
-    /// This must be called by the interrupt handler to avoid spurious timer events.
-    fn is_pending(&self) -> bool;
-
-    /// Obtains the current time as an `Instant`.
-    fn now(&self) -> u32;
-}
-
-// Taken from Rubble BLE timer implementation.
-impl Timer for TIMER0 {
-    unsafe fn duplicate(&self) -> Self {
-        mem::transmute_copy(self)
-    }
-
-    fn init(&mut self) {
-        self.bitmode.write(|w| w.bitmode()._32bit());
-        // 2^4 = 16
-        // 16 MHz / 16 = 1 MHz = µs resolution
-        self.prescaler.write(|w| unsafe { w.prescaler().bits(4) });
-        self.tasks_clear.write(|w| unsafe { w.bits(1) });
-        self.tasks_start.write(|w| unsafe { w.bits(1) });
-    }
-
-    fn set_interrupt(&mut self, at: u32) {
-        self.cc[1].write(|w| unsafe { w.bits(at) });
-        self.events_compare[1].reset();
-        self.intenset.write(|w| w.compare1().set());
-    }
-
-    fn clear_interrupt(&mut self) {
-        self.intenclr.write(|w| w.compare1().clear());
-        self.events_compare[1].reset();
-    }
-
-    fn is_pending(&self) -> bool {
-        self.events_compare[1].read().bits() == 1u32
-    }
-
-    fn now(&self) -> u32 {
-        self.tasks_capture[0].write(|w| unsafe { w.bits(1) });
-        let micros = self.cc[0].read().bits();
-        micros
-    }
-}
